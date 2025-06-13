1c1dec6bea274b41b2a34dcd30f16374889848a2
RuslanGocov <ruslan.gocov@midnight.works>
2025-06-13 10:32:57 +0300

    Change

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
 
-   // Coment
+
    private Vector3 offset = new Vector3(0f, 3f, -1f);
    private float smoothTime = 0.25f;
    private Vector3 velocity =  Vector3.zero;
